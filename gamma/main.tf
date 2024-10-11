dea3cbbf192f5bbc32bccb744b91d1edc1357cab
Veirt <veirt@duck.com>
2024-10-11 20:15:40 +0800

    reorganize and fix security groups

--- gamma/main.tf
+++ gamma/main.tf
@@ -1,68 +1,64 @@
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Security group to allow ssh connection"
+resource "aws_security_group" "allow_outgoing" {
+  name        = "allow_outgoing"
+  description = "Security group to allow all outgoing traffic"
 
-  # SSH
-  ingress {
+  # Allow all outgoing traffic
+  egress {
     from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_incoming" {
+  name        = "allow_incoming"
+  description = "Security group to allow various incoming connections"
+
+  # SSH (both ports 22 and 2269)
+  ingress {
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # SSH
   ingress {
-    from_port   = 0
+    from_port   = 2269
     to_port     = 2269
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-}
-
-resource "aws_security_group" "allow_outgoing" {
-  name        = "allow_outgoing"
-  description = "Security group to allow all outgoing"
-
-  # Allow all outgoing TCP traffic
-  egress {
-    from_port   = 0
-    to_port     = 65535
+  # HTTP, HTTPS, HTTP/3
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outgoing UDP traffic
-  egress {
-    from_port   = 0
-    to_port     = 65535
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-}
-
-resource "aws_security_group" "allow_wireguard" {
-  name        = "allow_wireguard"
-  description = "Security group to allow wireguard"
-
-
-  # Wireguard UDP
+  # WireGuard UDP
   ingress {
-    from_port   = 0
+    from_port   = 123
     to_port     = 123
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # # wg-easy Web
-  # ingress {
-  #   from_port   = 0
-  #   to_port     = 51820
-  #   protocol    = "udp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
 }
 
 resource "aws_instance" "gamma" {
@@ -77,8 +73,7 @@ resource "aws_instance" "gamma" {
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [
-    aws_security_group.allow_ssh.id,
-    aws_security_group.allow_outgoing.id,
-    aws_security_group.allow_wireguard.id
+    aws_security_group.allow_incoming.id,
+    aws_security_group.allow_outgoing.id
   ]
 }
